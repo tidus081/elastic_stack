0b0d252d86c94da3e95aedd415ae4a7b718d1c33
robbie <tidus081@naver.com>
2019-02-11 15:44:42 -0500

    add client functons

---
+++ install-client.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pip3 install python-logstash
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/5.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-5.x.list
+sudo apt-get update && sudo apt-get install logstash
+
+cp "logstash-simple.conf" "/etc/logstash/conf.d/logstash-client.conf"
+
+-u logstash /usr/share/logstash/bin/logstash --path.settings /etc/logstash -f /etc/logstash/conf.d/logstash-client.conf --config.test_and_exit
